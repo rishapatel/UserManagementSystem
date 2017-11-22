9aa50efe20ae8e54749ab7592ab01caeba7f3dcd
rishapatel <33834975+rishapatel@users.noreply.github.com>
2017-11-22 07:55:25 -0600

    Updated annotation  SpringBootApplication

--- src/main/java/com/javaaapplication/springboot/Application.java
+++ src/main/java/com/javaaapplication/springboot/Application.java
@@ -7,7 +7,12 @@ import org.springframework.context.annotation.Import;
 import com.javaaapplication.springboot.configuration.JpaConfiguration;
 
 @Import(JpaConfiguration.class)
-@SpringBootApplication(scanBasePackages={"com.javaaapplication.springboot"})
+@SpringBootApplication
+
+/*In case if SpringBootApplication is unable to scan base class ,Use:
+*@SpringBootApplication(scanBasePackages={"com.javaaapplication.springboot"})
+*/
+
 public class Application {
 
 	public static void main(String[] args) {
