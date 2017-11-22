ecf177c367f77b91153546632bc5a455de512ed0
rishapatel <33834975+rishapatel@users.noreply.github.com>
2017-11-22 07:32:15 -0600

    Updated class file definition

--- src/main/java/com/javaaapplication/springboot/controller/AppController.java
+++ src/main/java/com/javaaapplication/springboot/controller/AppController.java
@@ -1,5 +1,8 @@
 package com.javaaapplication.springboot.controller;
 
+/*
+* View handling controller
+*/
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
